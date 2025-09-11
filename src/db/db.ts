d64d1db795a86978e5c15f8443da5a293437237b
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2025-09-11 06:19:02 +0100

    update

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mydb");
+    await mongoose.connect(process.env.MONGODB_URL as string);
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
