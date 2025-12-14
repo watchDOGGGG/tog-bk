85f7ed6aca56f41ab45c78769dcb34bf27833a03
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2025-12-14 18:43:35 +0100

    update on file

--- src/game/trivia/index.ts
+++
@@ -1,7 +0,0 @@
-import { Server } from "socket.io";
-import { initTriviaSockets } from "./triviaGame";
-
-export const initTriviaGame = (io: Server) => {
-  console.log("🎮 Initializing Trivia Game...");
-  initTriviaSockets(io);
-};
